81c85eb6ea8620aeac34fdc83ea9489845b04bb6
maimohammed928 <maimohammed928@gmail.com>
2024-11-19 00:57:39 +0200

    second commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <PrivyProvider
+      appId="cm3n8ucub05816k46rs8aeqnz"
+      config={{
+        // Display email and wallet as login methods
+        loginMethods: ['email', 'wallet'],
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </React.StrictMode>,
+);
